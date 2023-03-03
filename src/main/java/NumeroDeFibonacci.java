b1ff0c2378f7546d68532630ce68a43d6a82d54d
dapihelg <xlx-1@hotmail.com>
2023-03-02 22:58:06 -0600

    Terminación del código recursivo

--- src/main/java/NumeroDeFibonacci.java
+++ src/main/java/NumeroDeFibonacci.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class NumeroDeFibonacci {
-    
+
+    public static int lim=0;
+    public static int x =0;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("límite de la serie ");
+        lim = in.nextInt();
+        System.out.println("La serie llegará hasta la posición "+lim);
+        lector(x, lim);
+
+    }
+public static int sumaSerie(int x){
+        if(x == 0 || x==1)
+            return 1;
+        else
+            return sumaSerie(x - 1) + sumaSerie(x - 2);
+    }
+    public static void lector(int x, int lim){
+            System.out.println(" indice: "+x+" número de la serie: "+sumaSerie(x));
+            x++;
+            if(x<=lim)
+        lector(x, lim);
+    }
 }
